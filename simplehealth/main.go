67a018feadbc18124366bf616fbcf8bcbd35910a
Rob Owens <rowens@incomm.com>
2019-07-18 14:19:48 -0400

    update flags

--- simplehealth/main.go
+++ simplehealth/main.go
@@ -1,5 +1,6 @@
 package main
 
+// https://gobyexample.com/command-line-flags
 import (
 	"flag"
 	"fmt"
@@ -12,22 +13,21 @@ func main() {
 	hostPtr := flag.String("host", "golang.org", "hostname or IP")
 	portPtr := flag.String("port", "80", "portnumber")
 	sleepPtr := flag.Int64("sleep", 5, "Polling interval in seconds")
-
+	timePtr := flag.Int64("timeout", 10, "TCP Timeout, default 10")
 	flag.Parse()
 
 	for {
-		conn, err := net.Dial("tcp", *hostPtr+":"+*portPtr)
+		timeout := time.Duration(*timePtr) * time.Second
+		conn, err := net.DialTimeout("tcp", *hostPtr+":"+*portPtr, timeout)
+
 		if err != nil {
 			fmt.Println(err)
 			return
-
 		}
-
 		fmt.Printf("Connection established between %s and localhost.\n", *hostPtr)
 		fmt.Printf("Remote Address : %s \n", conn.RemoteAddr().String())
 		fmt.Printf("Local Address : %s \n", conn.LocalAddr().String())
 		time.Sleep(time.Duration(*sleepPtr) * time.Second)
-
 	}
 
 }
