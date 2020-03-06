57f0bf6279adac73b5da8d9570dd0c3400110c10
Rob Owens <rowens@incomm.com>
2020-03-06 15:22:30 -0500

    sentienl

--- redis/sentinel.go
+++ redis/sentinel.go
@@ -9,18 +9,50 @@ import (
 
 func main() {
 
-	conn, err := redis.Dial("tcp", "10.3.234.100:10002", redis.DialPassword("cluster"))
+	connr, err := redis.Dial("tcp", "127.0.0.1:6379", redis.DialPassword("cluster"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
+	defer connr.Close()
 
-	role, err := redis.Strings(conn.Do("sentinel", "master", "OWENS"))
+	conns, err := redis.Dial("tcp", "127.0.0.1:10002", redis.DialPassword("cluster"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conns.Close()
+
+	//        conn.Send("ROLE")
+	//       if err != nil {
+	//             	log.Fatal(err)
+	//	}
+	//        conn.Send("ROLE")
+	//        conn.Send("GET", "foo")
+	//        conn.Flush()
+	//        one, two := conn.Receive()
+	//        fmt.Println("1: ",one,"2: ",two)
+	//	s, err := redis.String(conn.Do("GET", "foo"))
+	//	s, err := redis.String(conn.Do("KEYS", "*"))
+	//	fmt.Printf("%#v\n", s)
+	//
+	//role, err := redis.Strings(conn.Do("KEYS", "*"))
+	role, err := redis.Strings(connr.Do("ROLE"))
+	//role, err := redis.Strings(conns.Do("sentinel", "failover", "OWENS"))
 	if err != nil {
 		// handle error
 	}
 	for _, key := range role {
-		fmt.Println(key)
+		//	fmt.Println(key)
+		if key == "master" {
+			fmt.Println("Good Master")
+			return
+		} else if key == "slave" {
+			fmt.Println("Slave Failover initiated...")
+			conns.Send("sentinel", "failover", "OWENS")
+			conns.Flush()
+			a, b := conns.Receive()
+			_ = b
+			fmt.Println(a)
+		}
 	}
 }
-
